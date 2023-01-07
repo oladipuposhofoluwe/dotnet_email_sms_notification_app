c854c8aa961beb376f21d23f8c9027d3edbfebe3
oladipupo shofoluwe <oladipuposhofoluwe@gmail.com>
2023-01-07 16:07:57 +0100

    remove unecessary class

--- Contracts/IUserService.cs
+++
@@ -1,9 +0,0 @@
-// using NotificationApp.Dto;
-
-// namespace NotificationApp.Contracts
-// {
-//     public interface IUserService
-//     {
-//         public Task<string> RegisterUser(CreateUserDto userDto);
-//     }
-// }
